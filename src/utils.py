1c79fb98e83a7f3687962b91c5b18275d8d195b9
Adria Cabello <adria.cabello@chemotargets.com>
2024-01-09 15:38:21 +0100

    added crossval plots

--- src/utils.py
+++ src/utils.py
@@ -8,6 +8,7 @@ import seaborn as sns
 from IPython.display import display
 from pandas.api.types import CategoricalDtype
 
+from sklearn.model_selection import KFold, StratifiedKFold
 
 # Set Matplotlib defaults
 plt.style.use("seaborn-whitegrid")
@@ -35,3 +36,48 @@ def load_data():
     #all the data
     df = pd.concat([df_train, df_test])
     return df_train, df_test, df
+
+
+def plot_cross_val(n_splits: int, splitter_func, df: pd.DataFrame, target_column: str, title_text: str) -> None:
+    """Function to plot the cross-validation of various sklearn splitter objects."""
+    split = 1
+    plot_data = []
+    
+    if splitter_func == StratifiedKFold:
+        y = df[target_column]
+        X = df.drop(columns=[target_column])
+        for train_index, valid_index in splitter_func(n_splits=n_splits).split(X, y):
+            plot_data.append([train_index, 'Train', f'{split}'])
+            plot_data.append([valid_index, 'Test', f'{split}'])
+            split += 1
+    else:
+        for train_index, valid_index in splitter_func(n_splits=n_splits).split(df):
+            plot_data.append([train_index, 'Train', f'{split}'])
+            plot_data.append([valid_index, 'Test', f'{split}'])
+            split += 1
+
+    plot_df = pd.DataFrame(plot_data, columns=['Index', 'Dataset', 'Split']).explode('Index')
+
+    plt.figure(figsize=(10, 6))
+    handles = []
+    labels = []
+    
+    train_handles = plt.scatter([], [], label='Train', color='blue', alpha=0.7)
+    test_handles = plt.scatter([], [], label='Test', color='goldenrod', alpha=0.7)
+    
+    for split_num, group in plot_df.groupby('Split'):
+        train_indices = group['Index'][group['Dataset'] == 'Train']
+        test_indices = group['Index'][group['Dataset'] == 'Test']
+
+        plt.scatter(train_indices, [split_num] * len(train_indices), color='blue', alpha=0.7)
+        plt.scatter(test_indices, [split_num] * len(test_indices), color='goldenrod', alpha=0.7)
+    
+    handles.extend([train_handles, test_handles])
+    labels.extend(['Train', 'Test'])
+
+    plt.xlabel('Index')
+    plt.ylabel('Split')
+    plt.title(title_text)
+    plt.legend(handles, labels)
+    plt.grid(True)
+    plt.show()
